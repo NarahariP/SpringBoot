902197ac8ce798632356b611a2a655d3aedae66f
Narahari <Narahari@192.168.0.6>
2019-11-26 21:34:45 +0530

    Initial Commit.

---
+++ SpringBootApplicaiton/src/main/java/com/hari/boot/SpringBootApplicaiton/SpringBootApplicaitonApplication.java
@@ -0,0 +1,13 @@
+package com.hari.boot.SpringBootApplicaiton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicaitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicaitonApplication.class, args);
+	}
+
+}
