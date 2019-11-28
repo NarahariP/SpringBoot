633e0a47d818bcab2c339e71cd0ab3d9a7572183
Narahari <Narahari@192.168.0.5>
2019-11-28 22:33:31 +0530

    test

--- SpringBootLoadDataFromService/src/main/java/com/hari/SpringBootApplicaiton.java
+++ SpringBootLoadDataFromService/src/main/java/com/hari/SpringBootApplicaiton.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootApplicaiton {
 
 	public static void main(String[] args) {
-		System.out.println("Application >> Starting....");
+		System.out.println("Application >> Starting.....");
 		SpringApplication.run(SpringBootApplicaiton.class, args);
 	}
 
