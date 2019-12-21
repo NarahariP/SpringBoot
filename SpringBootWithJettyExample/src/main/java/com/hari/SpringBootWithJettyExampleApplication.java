437ce8146ef56fc466d7147c7960f5716740f50a
Narahari <Narahari@192.168.0.6>
2019-12-21 20:06:46 +0530

    commit;

---
+++ SpringBootWithJettyExample/src/main/java/com/hari/SpringBootWithJettyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJettyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJettyExampleApplication.class, args);
+	}
+
+}
